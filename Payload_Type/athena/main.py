de2c66d56e5a5035926f1d60c17f82a76bfc5755
checkymander <scottie.austin1991@gmail.com>
2023-07-12 12:50:00 -0400

    add restore?

--- Payload_Type/athena/main.py
+++ Payload_Type/athena/main.py
@@ -2,6 +2,7 @@ import mythic_container
 from athena.mythic import *
 import subprocess
 
+p = subprocess.Popen(["dotnet", "restore"], cwd="/Mythic/athena/agent_code/AthenaPlugins")
 p = subprocess.Popen(["dotnet", "msbuild", "-m", "/consoleloggerparameters:ErrorsOnly" "--verbosity=q", "--nologo"], cwd="/Mythic/athena/agent_code/AthenaPlugins")
 
 mythic_container.mythic_service.start_and_run_forever()
