3a1df9270a009a53c7ba7e07a40811209fa7e5b6
checkymander <scottie.austin1991@gmail.com>
2023-07-12 12:51:17 -0400

    asdf

--- Payload_Type/athena/main.py
+++ Payload_Type/athena/main.py
@@ -2,7 +2,6 @@ import mythic_container
 from athena.mythic import *
 import subprocess
 
-p = subprocess.Popen(["dotnet", "restore"], cwd="/Mythic/athena/agent_code/AthenaPlugins")
-p = subprocess.Popen(["dotnet", "msbuild", "-m", "/consoleloggerparameters:ErrorsOnly" "--verbosity=q", "--nologo"], cwd="/Mythic/athena/agent_code/AthenaPlugins")
+p = subprocess.Popen(["dotnet", "msbuild", "-m", "/consoleloggerparameters:ErrorsOnly" "--verbosity=q", "--nologo", "-t:restore", "-p:RestorePackagesConfig=true"], cwd="/Mythic/athena/agent_code/AthenaPlugins")
 
 mythic_container.mythic_service.start_and_run_forever()
